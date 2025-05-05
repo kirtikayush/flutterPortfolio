0d7ec0cf050f5c4ddfa23494e697b7356bf257b4
kirtikayush <167364209+kirtikayush@users.noreply.github.com>
2025-05-05 15:57:14 +0530

    Name together Mobile

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3053085797"
+    serviceWorkerVersion: "3587928095"
   }
 });
