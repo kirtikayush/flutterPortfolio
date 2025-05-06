014132dfbdf14fcd1583e2b483c145b05e9f6773
kirtikayush <kirtikayush2000@gmail.com>
2025-05-06 22:36:34 +0530

    changes 10:35pm, 6/5/25

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2286417491"
+    serviceWorkerVersion: "3072789289"
   }
 });
